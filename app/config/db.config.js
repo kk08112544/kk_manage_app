50af4f0caf9db01d93ff135771a78093fb6fa008
CodeName2020 <pharisi098@gmail.com>
2023-10-03 09:42:29 +0700

    try commit

---
+++ app/config/db.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "",
+  // DB: "myapi"
+  HOST: "bcox7iq8ysidkvxknrhx-mysql.services.clever-cloud.com",
+  USER: "",
+  PASSWORD: "",
+  DB: "bcox7iq8ysidkvxknrhxbcox7iq8ysidkvxknrhx",
+};
